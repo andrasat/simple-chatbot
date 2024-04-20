bfc5ad0d23b58b5004fb837c77a03342c0df94da
andra <andra.satria1@gmail.com>
2024-04-21 00:46:32 +0700

    initiate frontend nextjs app

---
+++ app/frontend/next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  productionBrowserSourceMaps: false,
+  compress: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  reactStrictMode: true,
+};
+
+export default nextConfig;
